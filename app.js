cec4bfbdf74717483023ef440e494d02d9ed1b59
Kevin Qi <iqnivek@gmail.com>
2019-10-18 15:01:32 -0700

    add route, nodemon

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello world'));
 
+// Parse incoming request JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+const router = express.Router();
+router.get('/location', (req, res) => {
+  const name = req.query.name;
+
+  return res.status(200).json({
+    name,
+  });
+});
+app.use('/api', router);
+
+
+// Listen
 const port = 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
