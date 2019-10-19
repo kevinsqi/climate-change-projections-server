6d65a5af973dcb733c463475b66d3c2383ff76db
Kevin Qi <iqnivek@gmail.com>
2019-10-18 18:43:32 -0700

    use nearest neighbors query to search up temperature results

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+const knex = require('./db/knex');
 
 // Parse incoming request JSON
 app.use(express.json());
@@ -9,10 +10,26 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 const router = express.Router();
 router.get('/location', (req, res) => {
-  const name = req.query.name;
+  const { lat, lon } = req.query;
 
-  return res.status(200).json({
-    name,
+  if (lat == null || lon == null) {
+    return res.status(400).json({
+      error: 'LAT_AND_LON_REQUIRED',
+    });
+  }
+
+  knex.raw(`
+    SELECT * FROM temperatures_cmip5
+    ORDER BY geography <-> 'SRID=4326;POINT(-122.401780 37.797650)'
+    LIMIT 1
+  `).then((result) => {
+    return res.status(200).json({
+      query: {
+        lat: lat,
+        lon: lon,
+      },
+      result: result.rows[0],
+    });
   });
 });
 app.use('/api', router);
